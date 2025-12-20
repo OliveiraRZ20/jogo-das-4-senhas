530e5239b18c95f495d0682b1bc33714ec73e09c
Lucca Oliveira <lucca0505@gmail.com>
2025-12-20 19:20:49 -0300

    Feat: Create terminal.py
    Criação do arquivo que fará controle de execução, e limpeza do terminal

---
+++ game/utils/terminal.py
@@ -0,0 +1,20 @@
+# imports internos
+from game.utils.logger import alertar, informar, confirmar
+
+# imports externos
+from os import system, name
+
+
+def cls():
+    """Limpa a tela"""
+    system("cls" if name == "nt" else "clear")
+
+def pause():
+    """Pausa a execução"""
+    input("\nPressione a tecla 'Enter' para continuar...")
+
+def finalizar_programa():
+    """Finaliza o programa"""
+    print("")
+    confirmar("Programa finalizado com sucesso!")
+    exit(0)
